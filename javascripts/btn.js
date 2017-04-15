19ee3206fe6d78cc0bc11b9b25b87f67147a56e4
Chaz Henricks <chazhenricks@gmail.com>
2017-04-15 11:22:53 -0500

    basic styleing and blog post content

---
+++ javascripts/btn.js
@@ -0,0 +1,8 @@
+var enterButton = document.getElementById("enter");
+
+
+enterButton.addEventListener("click", navigate);
+
+function navigate(){
+  window.location.href = "file:///Users/chazhenricks/workspace/the-static-web/milestone-challenge/blog.html";
+}
