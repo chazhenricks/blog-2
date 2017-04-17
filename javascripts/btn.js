ebe72627c1eb3ad9e879ba668093ca67815c149c
Chaz Henricks <chazhenricks@gmail.com>
2017-04-17 13:20:08 -0500

    fixed filepath for index button

--- javascripts/btn.js
+++ javascripts/btn.js
@@ -4,5 +4,5 @@ var enterButton = document.getElementById("enter");
 enterButton.addEventListener("click", navigate);
 
 function navigate(){
-  window.location.href = "file:///Users/chazhenricks/workspace/the-static-web/milestone-challenge/blog.html";
+  window.location.href = "blog.html";
 }
