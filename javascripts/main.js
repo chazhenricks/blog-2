36e8e2bc2404ede9a1499359efd4d800fbc5d4b3
Chaz Henricks <chazhenricks@gmail.com>
2017-05-05 11:06:27 -0500

    changed load order of blog posts so most recent is at top

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,14 +2,14 @@ var blogPostContent = $('#blog-posts');
 var links = $('#list-group');
 
 $.ajax({
-  url: "blog-posts.JSON"
-   }).done(loadBlogPosts);
+    url: "blog-posts.JSON"
+}).done(loadBlogPosts);
 
-function loadBlogPosts(posts){
-  var blogPosts = posts.blogs;
-  for (var i=0;i<blogPosts.length;i++){
-  var posts =
-    `<article class ="container-fluid">
+function loadBlogPosts(posts) {
+    var blogPosts = posts.blogs;
+    for (var i = 0; i < blogPosts.length; i++) {
+        var posts =
+            `<article class ="container-fluid">
       <header class="panel-header">
         <h3 class="">${blogPosts[i].date}</h3>
         <h2 id ="post${i}">${blogPosts[i].title} </h2>
@@ -22,60 +22,15 @@ function loadBlogPosts(posts){
       </footer>
     </article>`;
 
-  var linkContent =
-  `<a href="#post${i}" class="list-group-item">
+        var linkContent =
+            `<a href="#post${i}" class="list-group-item">
     <h4 class ="list-group-item-heading">${blogPosts[i].title}</h4>
     <p class ="list-group-item-text">${blogPosts[i].author}</p>
    </a>
   `
 
- blogPostContent.append(posts);
-  links.append(linkContent);
-  }
+        blogPostContent.prepend(posts);
+        links.append(linkContent);
+    }
 };
-
-
-
-
-
-
-
-
-
-// var container = document.getElementById("container");
-// var links = document.getElementById("list-group");
-
-// for (var i=0;i<blogPosts.length;i++){
-//   var posts =
-//     `<article class ="container-fluid">
-//       <header class="page-header">
-//         <h3 class="">${blogPosts[i].date}</h3>
-//         <h2 id ="post${i}">${blogPosts[i].title} </h2>
-//       </header>
-//       <section class="">
-//         ${blogPosts[i].copy}
-//       </section>
-//       <footer class="">
-//         <p class="">Written By - ${blogPosts[i].author}
-//       </footer>
-//     </article>`;
-
-//   var linkContent =
-//   `<a href="#post${i}" class="list-group-item">
-//     <h4 class ="list-group-item-heading">${blogPosts[i].title}</h4>
-//     <p class ="list-group-item-text">${blogPosts[i].author}</p>
-//    </a>
-//   `
-
-// container.innerHTML += posts;
-// links.innerHTML += linkContent;
-
-// };
-
-
-
-
-
-
-
 
