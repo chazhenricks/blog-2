46877962c62cf31cc6149d0e5a04a8d2648084d1
Chaz Henricks <chazhenricks@gmail.com>
2017-05-05 10:42:25 -0500

    made into SPA and added blog population from JSON file

--- javascripts/btn.js
+++ javascripts/btn.js
@@ -1,8 +1,45 @@
-var enterButton = document.getElementById("enter");
+console.log("BTN JS");
+
+//Content
+var blogContent = $('#blog-content');
+var aboutContent = $('#about-content');
+var portContent = $('#portfolio-content');
+
+//Homepage enter button
+var enterBtn = $('#enter');
+var navBar = $("#nav-bar");
+
+enterBtn.click(function(){
+    blogContent.removeClass("hidden");
+    navBar.removeClass('hidden');
+    enterBtn.addClass("hidden");
+});
+
+//Navbar Navigation
+var blogNav = $('#blog-nav');
+var aboutNav = $('#about-nav');
+var portNav = $('#portfolio-nav');
+
+blogNav.click(function(){
+    blogContent.removeClass("hidden");
+    aboutContent.addClass("hidden");
+    portContent.addClass("hidden");
+});
+
+aboutNav.click(function(){
+    aboutContent.removeClass("hidden");
+    blogContent.addClass("hidden");
+    portContent.addClass("hidden");
+});
+
+portNav.click(function(){
+    portContent.removeClass("hidden");
+    aboutContent.addClass("hidden");
+    blogContent.addClass("hidden");
+});
 
 
-enterButton.addEventListener("click", navigate);
 
-function navigate(){
-  window.location.href = "blog.html";
-}
+
+
+
